29892a6270ffd027f19d048b45b314ae8fdffac8
Mustafa Koçer <107768698+mkocerim@users.noreply.github.com>
2022-12-16 08:44:33 +0300

    authSlice

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -11,8 +11,23 @@ export const authSlice=createSlice({
     name: 'authSlice',
     initialState,
     reducers:{
+
+    setToken:(state,action)=>{
+
+    },
+    removeToken:(state,action)=>{
+
+    },
     
     // TODO burayı birazdan olustur
 
     }
 })
+
+
+export default authSlice.reducer
+
+export const{setToken,removeToken}= authSlice.actions
+
+//export const {setToken} = authSlice.actions
+//export const removeToken= authSlice.actions.removeToken
